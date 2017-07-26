62c5452153ed992491aebca85d1cc949207fefe5
aduvivier <aurelienduvivier@rocketmail.com>
2017-07-26 10:39:53 +0200

    commit

--- .script/installpackages.sh
+++ .script/installpackages.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -x
 
+MASTERUSER="user"
+MASTERPASSWORD="password"
+ENDPOINT='proddb.cqlsysjscfhb.eu-west-1.rds.amazonaws.com'
+
+yum install -y nginx mysql php php-mysql php-fpm php-gd php-mbstring php-mcrypt
+
 cd /opt
 
 wget https://wordpress.org/latest.tar.gz
@@ -9,4 +15,6 @@ tar -xvzf latest.tar.gz -C /usr/share/nginx/html/
 chown -R nginx:nginx /usr/share/nginx/html/wordpress
 
 chmod -R 755 /usr/share/nginx/html/wordpress
+
+mysql -u $MASTERUSER --password=$MASTERPASSWORD --host=$ENDPOINT < /usr/share/nginx/html/wordpress/setup.mysql
 
