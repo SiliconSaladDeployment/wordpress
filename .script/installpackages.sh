b7bfc003be4049eb8eb0d341aa1323cb5fdb9e25
aduvivier <aurelienduvivier@rocketmail.com>
2017-07-26 11:18:47 +0200

    commit

--- .script/installpackages.sh
+++ .script/installpackages.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -x
 
-MASTERUSER="user"
-MASTERPASSWORD="password"
-ENDPOINT='proddb.cqlsysjscfhb.eu-west-1.rds.amazonaws.com'
-
 yum install -y nginx mysql php php-mysql php-fpm php-gd php-mbstring php-mcrypt
 
 cd /opt
@@ -15,7 +11,3 @@ tar -xvzf latest.tar.gz -C /usr/share/nginx/html/
 chown -R nginx:nginx /usr/share/nginx/html/wordpress
 
 chmod -R 755 /usr/share/nginx/html/wordpress
-
-mysql -u $MASTERUSER --password=$MASTERPASSWORD --host=$ENDPOINT < /usr/share/nginx/html/wordpress/setup.mysql
-
-cat /usr/share/nginx/html/wordpress/nginx.conf > /etc/nginx/nginx.conf
--- .script/restartnginx.sh
+++ .script/restartnginx.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
+MASTERUSER='user'
+MASTERPASSWORD='password'
+ENDPOINT='proddb.cqlsysjscfhb.eu-west-1.rds.amazonaws.com'
+
+mysql -u $MASTERUSER --password=$MASTERPASSWORD --host=$ENDPOINT < /usr/share/nginx/html/wordpress/setup.mysql
+
+cat /usr/share/nginx/html/wordpress/nginx.conf > /etc/nginx/nginx.conf
+
 service nginx restart
