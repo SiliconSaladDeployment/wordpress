dc951d788879fc7b572d9e892cc7ed43b1d32ed2
aduvivier <aurelienduvivier@rocketmail.com>
2017-07-26 15:24:49 +0200

    commit

--- wp-config.php
+++ wp-config.php
@@ -1,7 +1,7 @@
 <?php
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'wordpressdb');
 
 /** MySQL database username */
 define('DB_USER', 'user');
